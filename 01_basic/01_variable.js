d58de0dbea355e350caefd060d87d6de06ada6da
Nitesh yadav <109825110+niteshyadav07@users.noreply.github.com>
2025-03-20 18:22:41 +0000

    datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = "223344"
+let accountEmail = "nitesh@gmail.com"
+var accountpass = "1234"
+ 
+accountcity = "gaur"
+
+console.log(accountEmail)
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountEmail,accountId,accountpass,accountcity])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+
+let  age = 18
+let islogedin = false
+let state;
+
+//number => 2 to power 53
+//null => standalone value
+//bigint
+//string=""
+//bollean => true/false
+//undefined
+//symbol => unique
+
+
+//object
+console.log(typeof age)
