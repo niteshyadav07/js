1fba4a8d4a83131d0f04aa7e66557e1bda62f2f3
Nitesh yadav <109825110+niteshyadav07@users.noreply.github.com>
2025-03-24 18:08:37 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+//console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//"33"=>33
+//"33abc"=>NaN
+//true => 1; false =>0
+
+let isloggedin = 1;
+
+let booleanisloggedin = Boolean(isloggedin)
+//console.log(booleanisloggedin)
+// 1 => true; 0=> false
+// "" => false
+// "nitesh" => true
+
+let  somenumber = 33;
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
