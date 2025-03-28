75c2921c559d766b0a22c6fabb103820e66495a2
Nitesh yadav <109825110+niteshyadav07@users.noreply.github.com>
2025-03-28 16:04:40 +0000

    nums and math

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,41 @@
+const  score = 400;
+
+//console.log(score);
+
+const balance = new Number(100);
+
+//console.log(balance);
+
+//console.log(balance.toString().length);
+
+////console.log(balance.toFixed(1));
+
+const othernum =  23.8966
+
+//console.log(othernum.toPrecision(3));
+
+const hundred = 1000000;
+
+//onsole.log(hundred.toLocaleString('en-IN'));
+
+//---------------------maths--------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,2,3,5,1));
+// console.log(Math.max(2,3,4,5,6,7));
+console.log(Math.random());
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10 ;
+const max = 20;
+
+ 
+
+
+
