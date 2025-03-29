1a5ecf59a3228663f3078519ce0865866083912d
Nitesh yadav <109825110+niteshyadav07@users.noreply.github.com>
2025-03-29 06:09:56 +0000

    date

---
+++ 01_basic/07_datesin.js
@@ -0,0 +1,10 @@
+//
+
+let mydate = new Date();
+console.log(mydate.toString());
+console.log((mydate.toDateString()));
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+
+
+let myCreateate = new Date(2023,0,25 )
