fbcd27763e3445ba318fd177d289c96237aa62a9
Nitesh yadav <109825110+niteshyadav07@users.noreply.github.com>
2025-03-29 18:55:58 +0000

    date

--- 01_basic/07_datesin.js
+++ 01_basic/07_datesin.js
@@ -7,4 +7,28 @@ console.log(mydate.toLocaleString());
 console.log(typeof mydate);
 
 
-let myCreateate = new Date(2023,0,25 )
+//et myCreateate = new Date(2023,0,23,5,3);
+
+let myCreateate = new Date("14-01-2023");
+
+//console.log(myCreateate.toLocaleString())
+
+let mytimestamp = Date.now();
+
+// console.log(mytimestamp);
+// console.log(myCreateate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date ();
+
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay())
+
+//'${newDate.getDay()}' and the time
+
+newDate.toLocaleDateString('default',{
+    weekday: "long",
+    
+})
