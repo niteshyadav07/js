b4e27975411f9093a6c11092e3fabdca736f2a69
Nitesh yadav <109825110+niteshyadav07@users.noreply.github.com>
2025-04-02 16:00:18 +0000

    method

--- 02_basic/01_arrays.js
+++ 02_basic/01_arrays.js
@@ -3,5 +3,29 @@
 const  myarr=[0,1,2,3,4,5]
 
 const myhero =["srk","sk","rk"]
+
+const myarr2 = new Array(1,2,3,4,5)
 console.log(myarr[0]);
 console.log(myhero[1])
+
+
+///  Array methods 
+
+// myarr.push(6,7)
+// myarr.pop()
+
+//myarr.unshift(0)
+//myarr.shift()
+
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(3));
+
+// const newarr = myarr.join();
+
+// console.log(myarr);
+// console.log(typeof newarr);
+
+
+// slice, splice
+
+log
