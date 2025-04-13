9931c12489759c0ecb000f4b24dc4bdc16cc763c
Nitesh yadav <109825110+niteshyadav07@users.noreply.github.com>
2025-04-13 15:45:33 +0000

    function

---
+++ 03_basic/01_functions.js
@@ -0,0 +1,17 @@
+function saymyname (){
+    console.log("n");
+    console.log("i");
+    console.log("t");
+    console.log("e");
+    console.log("s");
+    console.log("h");
+}
+
+//saymyname()
+
+
+function addtwonum (num1,num2){
+    console.log( num1 + num2);
+}
+
+addtwonum(3,"a")
